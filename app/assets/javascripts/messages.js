80807969a8a2e429cce12e5aeec711c0750cad42
bcn0024 <bcn0024@icloud.com>
2019-11-07 21:17:15 +0900

    aa

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -75,6 +75,9 @@ $(function () {
       })
 
       .done(function (messages) { //通信成功したら、controllerから受け取ったデータ（messages)を引数にとって以下のことを行う
+        if (message.length == 0 ){
+          return false;
+        }
         var insertHTML = '';//追加するHTMLの入れ物を作る
         messages.forEach(function (message) {//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
           insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
